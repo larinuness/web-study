77bd1844441847fed5c480a379bf59a5a6a06869
Larissa dos Santos Nunes - LNN <larissa.nunes@iteris.com.br>
2023-08-11 11:37:55 -0300

    feat: seletores

--- estudos-html-css/css/js/tag.js
+++ estudos-html-css/css/js/tag.js
@@ -3,20 +3,35 @@ const colors = {
     div: '#1565c0',
     span: '#e53935',
     section: '#f67809',
+    ul: '#5e34b1',
+    ol: '#fbc02d',
+    header: '#d81b60',
+    nav: '#64dd17',
+    main: '#00acc1',
+    footer: '#304ffe',
+    form: '#9f6581',
+    body: '#25b6da',
+    padrao: '#616161',
+    get(tag) {
+        return this[tag] ? this[tag] : this.padrao
+    }
     
 
 
 }
-
+//pega o que ta marcado com a classe '.tag'
 document.querySelectorAll('.tag').forEach(elemento => {
+    //pega o nome da tag e deixa em minusculo
     const tagName = elemento.tagName.toLowerCase()
-    elemento.style.borderColor = '#616161'
-
+    //aplicar uma cor na borda
+    elemento.style.borderColor = colors.get(tagName)
+    
+    //se não tiver marcado como no label cai aqui
     if(!elemento.classList.contains('noLabel')) {
         const label = document.createElement('label')
-        label.style.backgroundColor = "#616161"
+        label.style.backgroundColor = colors.get(tagName)
         label.innerHTML = tagName;
-
+        //add antes do primeiro filho
         elemento.insertBefore(label, elemento.childNodes[0]);
     }
 })
